f8edecb80ee862017908e6eaaeb337052c2ae5ae
DassiGit <hzuckerto@gmail.com>
2026-06-18 18:10:00 +0300

    renaming repo the right way

---
+++ html/48/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+
+namespace Homework
+{
+    internal class Program
+    {
+
+        
+       static void Main(string[] args)
+        {   string[] colors = { "red", "green", "blue" };
+             string[] patterns = { "striped", "checked", "plain" };
+            foreach (string s in colors) {
+                foreach (string p in patterns)
+                {
+                    new Shirt(s,p);
+                    Console.WriteLine($"This shirt is {s} and {p}.");
+                }
+
+                }
+
+        }
+    }
+
+    internal class Shirt
+    {   public string color;
+        public string pattern;
+
+        public Shirt(string color, string pattern)
+        {
+            this.color = color;
+            this.pattern = pattern;
+        }
+        
+    }
+}
